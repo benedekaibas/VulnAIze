4838676a68056fc5acf480d58a9a68abada126a6
benedekaibas <kaibas.benedek02@gmail.com>
2025-04-20 21:51:16 -0500

    started extracting the larger dataset to the model

---
+++ PoC/AI-Analyzer/extract_dataset.py
@@ -0,0 +1,33 @@
+import os 
+import shutil
+
+SRC_DIR = "dataset"
+DESTINATION_DIR = "proto_dataset"
+NUM_FILES_PER_CWE = 40
+
+# make the destination folder for storing data
+if not os.path.exists(DESTINATION_DIR):
+    os.makedirs(DESTINATION_DIR)
+
+def copy_example(cwe_folder):
+    good_code = []
+    vulnerable_code = []
+
+    for root, _, files in os.walk(cwe_folder):
+        for file in files:
+            full_path = os.path.join(root, file)
+            if "good" in file.lower():
+                good_code.append(full_path)
+            elif "bad" in file.lower():
+                vulnerable_code.append(full_path)
+    
+    good_code = good_code[:NUM_FILES_PER_CWE // 2]
+    vulnerable_code = vulnerable_code[:NUM_FILES_PER_CWE // 2]
+
+    for src_file in good_code + vulnerable_code:
+        label = "safe" if "good" in src_file else "vulnerable"
+        target_dir = os.path.join(DESTINATION_DIR, label)
+        os.makedirs(target_dir, exist_ok=True)
+        shutil.copy(src_file, os.path.join(target_dir, os.path.basename(src_file)))
+    
+    
